59ac795703bc3f490139296271e6b92374448546
mark <mark77234@naver.com>
2025-03-06 10:35:43 +0900

    fix: 필요없는 import 제거

--- app/src/main/java/com/example/chatapp/ChatPage.kt
+++ app/src/main/java/com/example/chatapp/ChatPage.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Send
-import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
